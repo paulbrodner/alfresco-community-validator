0b2638ff9785247f2f5154156c4cfa589b8ccab3
Paul Brodner <paul.brodner@ness.com>
2017-12-21 13:47:08 +0200

    adding preparation goal and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e  # exit if commands fails
 
-mvn --batch-mode -Darguments="-Dmaven.test.skip=true" clean release:prepare release:perform
+mvn --batch-mode -Darguments="-Dmaven.test.skip=true" -DskipTests release:prepare release:perform
